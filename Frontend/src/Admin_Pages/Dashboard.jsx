63b87e80a13ae40c18a9d225e598adfed10f8a85
Aadityaa45 <aaditya.joshi@avantika.edu.in>
2026-06-04 14:58:19 +0530

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS dependencies to package.json and configured Vite to use Tailwind.
    - Removed App.css and migrated styles to Tailwind utility classes.
    - Updated App.jsx to implement routing with react-router-dom and added a Dashboard component.
    - Modified main.jsx to wrap the App component with BrowserRouter for routing support.

---
+++ Frontend/src/Admin_Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const Dashboard = () =>{
+    return(
+        <div>
+            <h1 className='text-3xl font-bold underline'>Welcome to the Dashboard</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
